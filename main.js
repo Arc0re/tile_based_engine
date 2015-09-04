e122847c844e6d15092ef6c8e19b7e4913629053
Arc0re <paperboycheero@yahoo.com>
2015-09-04 16:07:34 +0200

    tile engine now working

--- main.js
+++ main.js
@@ -1,74 +1,47 @@
 // --- LOADING, SETUP --- //
 
-
-
-// Create the canvas
+// Creating the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 512;
 canvas.height = 480;
+ctx.imageSmoothingEnabled = false; // FOR PIXEL SCALING YEAH
 document.body.appendChild(canvas);
 
-// Loading assets
 
+// Creating and loading assets
+function Tile(src) {
+	this.image = new Image();
+	this.image.src = src;
+}
 
+black_tile = new Tile('assets/tiles/black_tile.png');
+forest_tile = new Tile('assets/tiles/forest_tile.png');
+grass_tile = new Tile('assets/tiles/grass_tile.png');
+town_tile = new Tile('assets/tiles/town_tile.png');
+water_tile = new Tile('assets/tiles/water_tile.png');
+hero_tile = new Tile('assets/hero.png');
 
-/* Levels */
-var map01Ready = false;
-var map01Image = new Image();
-map01Image.onload = function() {
-	map01Ready = true;
-};
-map01Image.src = "assets/maps/map01.png";
-
-var map02Ready = false;
-var map02Image = new Image();
-map02Image.onload = function() {
-	map02Ready = true;
-};
-map02Image.src = "assets/maps/map02.png";
-var map02X = 0;
-var map02Y = 0;
-var map02Speed = 500;
-
-var bigmapReady = false;
-var bigmapImage = new Image();
-bigmapImage.onload = function() {
-	bigmapReady = true;
-};
-bigmapImage.src = "assets/maps/bigmap.png";
-var bigmapX = 0;
-var bigmapY = 0;
-var bigmapSpeed = /*150*/500;
-
-
-
-/* Characters */
-var heroReady = false;
-var heroImage = new Image();
-heroImage.onload = function() {
-	heroReady = true;
-};
-heroImage.src = "assets/hero.png";
-
-/* Game objects */
-var hero = {
-	speed: 256,
-	x: 0,
-	y: 0
-};
-
-/* New Game */
-var game = function() {
-	hero.x = canvas.width / 2;
-	hero.y = canvas.height / 2;
-};
-
+var worldMap = [
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,2,2,2,2,2,2,2,2,2,2,2,2,0,0],
+	[0,0,2,2,2,2,2,2,2,2,2,2,2,2,0,0],
+	[0,0,2,2,2,2,2,2,2,2,2,2,2,2,0,0],
+	[0,0,1,2,1,2,1,1,1,1,1,1,1,1,0,0],
+	[0,0,1,1,1,1,1,1,3,1,1,1,1,1,0,0],
+	[0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0],
+	[0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0],
+	[0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0],
+	[0,0,3,1,1,2,1,1,1,1,1,1,1,1,1,0],
+	[0,0,1,2,1,2,1,1,1,1,1,1,1,1,0,0],
+	[0,0,1,2,1,2,1,1,1,1,1,1,1,1,0,0],
+	[0,0,2,2,2,2,2,2,2,2,2,2,2,2,0,0],
+	[0,0,2,2,2,2,2,2,2,2,2,2,2,2,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+];
 
 
 // --- INPUTS, CONTROLS, UPDATE --- //
-
-
 
 // Get keyboard inputs
 var keysDown = {};
@@ -81,13 +54,11 @@ addEventListener("keyup", function(event) {
 	delete keysDown[event.keyCode]; // Or = false
 }, false);
 
-// Update
 
-var currentMap = "bigmap";
+// Update
 var update = function(modifier) {
 	
-	// Controls when in world map
-	var worldControls = function() {
+	/*var worldControls = function() {
 		if (38 in keysDown) { // UP
 			if (hero.y >= bigmapY) {
 				bigmapY += bigmapSpeed * modifier;
@@ -109,82 +80,38 @@ var update = function(modifier) {
 			}
 		}
 	};
-	
-	// Controls when inside a house, cave, etc.
-	var houseControls = function() {
-		if (38 in keysDown) { // UP
-			//houseY += houseSpeed * modifier;
-			if (hero.y >= map02Y) {
-				map02Y += map02Speed * modifier;	
-			}
-		}
-		/*if (40 in keysDown) { // DOWN
-			if (houseY > - 688) {
-				houseY -= houseSpeed * modifier;			
-			}
-		}
-		if (37 in keysDown) { // LEFT
-			if (hero.x >= houseX) {
-				houseX += houseSpeed * modifier;
-			}
-		}
-		if (39 in keysDown) { // RIGHT
-			if (houseX > -740) {
-				houseX -= houseSpeed * modifier;
-			}
-		}*/
-	};
-	
-	/*console.log("map02X: " + map02X);
-	console.log("map02Y: " + map02Y);*/
-	
-	if (currentMap == "bigmap") {
-		worldControls();
-	} else if (currentMap == "map02") {
-		houseControls();
-	}
-	
-	//console.log(bigmapX + " " + bigmapY);
-	if ( (bigmapX < 258 && bigmapX > 40) && (bigmapY < 246 && bigmapY > 92) ) {
-		currentMap = "map02";
-	}
-	
+	*/
 	
 };
 
 
-
 // --- RENDERING --- //
 
-
 var render = function() {
-	
-	// Map loads
-	
-	/* Permanent black tile */
-	if (map01Ready) {
-		ctx.drawImage(map01Image, 0, 0);
-	}
-	
-	/* Houses, caves */
-	if (currentMap === "map01") {
-		if (map01Ready) {
-			ctx.drawImage(map01Image, 0, 0);
-		}
-	} else if (currentMap === "map02") {
-		if (map02Ready) {
-			ctx.drawImage(map02Image, map02X, map02Y);
-		}
-	} else if (currentMap === "bigmap") { /* World Map */
-		if (bigmapReady) {
-			ctx.drawImage(bigmapImage, bigmapX, bigmapY);
+		
+	for (var i = 0; i < worldMap.length; i++) {
+		for (var j = 0; j < worldMap[i].length; j++) {
+			if (worldMap[i][j] === 0) {
+				ctx.drawImage(water_tile.image, 0, 0, 32, 32, i*32, j*32, 32, 32);
+			}
+			if (worldMap[i][j] === 1) {
+				ctx.drawImage(grass_tile.image, 0, 0, 32, 32, i*32, j*32, 32, 32);
+			}
+			if (worldMap[i][j] === 2) {
+				ctx.drawImage(forest_tile.image, 0, 0, 32, 32, i*32, j*32, 32, 32);
+			}
+			if (worldMap[i][j] === 3) {
+				ctx.drawImage(town_tile.image, 0, 0, 32, 32, i*32, j*32, 32, 32);
+			}
 		}
 	}
-	
-	// Character loads
-	if (heroReady) {
-		ctx.drawImage(heroImage, hero.x, hero.y);
-	}
+	// for (var i = 0; i < canvas.width; i++) {
+	// 	for (var j = 0; j < canvas.height; j++) {
+	// 		// ctx.drawImage(grass_tile.image, (i * 32), (j * 32));
+	// 		ctx.drawImage(town_tile.image, 0, 0, 32, 32, i*64, j*64, 64, 64);
+	// 	}
+	// }
+	ctx.drawImage(hero_tile.image, 0, 0, 32, 32, (canvas.width/2)-32, (canvas.height/2)-32, 64, 64);
 };
 
 // --- MAIN GAME LOOP --- //
@@ -195,14 +122,11 @@ var main = function() {
 	
 	update(delta / 1000); // 1000 ms
 	render();
-	
 	then = now;
-	
 	requestAnimationFrame(main);
 };
 
 
 // Starting the bloody game
 var then = Date.now();
-game();
 main();
